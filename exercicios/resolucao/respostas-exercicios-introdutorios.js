772afa97fae725f644d7ed2f4257c6d1fbb80d6c
camilev07 <camilevalves07@gmail.com>
2025-10-08 19:59:24 -0300

    Alterações feitas dentro de sala de aula

--- exercicios/resolucao/respostas-exercicios-introdutorios.js
+++ exercicios/resolucao/respostas-exercicios-introdutorios.js
@@ -1,8 +1,50 @@
 // 1. Se ontem foi quarta-feira, amanhã será...
+console.log('1. Se ontem foi quarta-feira, amanhã será..')
+console.log('R: sexta-feira')
+console.log('### ------------------ ###')
+// 2. Complete a sequência: 1, 4, 9, 16, 25, ...
+console.log('2. Complete a sequência: 1, 4, 9, 16, 25, ...')
+console.log('R: 36')
+console.log('### ------------------ ###')
+// 3. Um trem de 100 metros de comprimento viaja a 50 km/h. Quanto tempo levará para atravessar um túnel de 50 metros?
+console.log ('3. Um trem de 100 metros de comprimento viaja a 50 km/h. Quanto tempo levará para atravessar um túnel de 50 metros?')
+// A velocidade de 50Km/h eh convertida p metros por segundo (m/s) dividindo por 3,6. 50/3.6 = 13,89
+// O tempo T eh calculado dividindo a distancia D pela velocidade V; 50/13,89 = 3,6
+// Ja que o trem tem 100m de comprimento e o tunel 50m, soma 150m; 150/13,89 = 10.8
+console.log ('R: 10.8')
+console.log('###----------------###')
+// 4. Júlia é mais alta que Ana. Ana é mais baixa que Bruno. Qual dos três é o mais alto?
+console.log('4. Júlia é mais alta que Ana. Ana é mais baixa que Bruno. Qual dos três é o mais alto?')
+console.log('R: Júlia ou Bruno')
+console.log('### ------------------ ###')
+// 5. Em uma sala, há 2 pais e 2 filhos. Qual o número mínimo de pessoas na sala?
+console.log('5. Em uma sala, há 2 pais e 2 filhos. Qual o número mínimo de pessoas na sala?')
+console.log('R: 3')
+console.log('### ------------------ ###')
+// 6. Se o código para "GATO" é 4130, qual é o código para "PATO"?
+console.log('6. Se o código para "GATO" é 4130, qual é o código para "PATO"?')
+console.log('R: 9130')
+console.log('### ------------------ ###')
+// 7. Você tem 50 moedas de 1 real e 50 moedas de 50 centavos. No total, quanto você tem?
+console.log('7. Você tem 50 moedas de 1 real e 50 moedas de 50 centavos. No total, quanto você tem?')
+console.log('R: 75')
+console.log('### ------------------ ###')
+// 8. Uma torneira pinga 1 gota a cada segundo. Quantas gotas ela pingará em 1 minuto?
+console.log('8. Uma torneira pinga 1 gota a cada segundo. Quantas gotas ela pingará em 1 minuto?')
+console.log('R: 60')
+console.log('### ------------------ ###')
+// 9. Se todos os corvos são pretos e minha caneta é um corvo, então minha caneta é preta. Essa afirmação é logicamente válida?
+console.log('9. Se todos os corvos são pretos e minha caneta é um corvo, então minha caneta é preta. Essa afirmação é logicamente válida?')
+console.log('R: Sim')
+console.log('### ------------------ ###')
+// 10. Se 3 coelhos comem 3 cenouras em 3 minutos, quanto tempo 10 coelhos levarão para comer 10 cenouras?
+console.log('10. Se 3 coelhos comem 3 cenouras em 3 minutos, quanto tempo 10 coelhos levarão para comer 10 cenouras?')
+console.log('R: 3 minutos')
+console.log('### ------------------ ###')
 
 
-//  o tamanho do meu array - 1, sempre será o índice máximo do meu array
-console.log('Índice máximo do array é: ' + Number(Array. length - 1))
 
-let email ='daniel@gmail.com' 
-if(email.trim() ==)
+
+
+
+
